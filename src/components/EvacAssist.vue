f604da473a8cc5dcdd0609a6266169f1469480cb
Trevor Leake <contact@trevorleake.com>
2019-07-06 10:10:13 -0700

    Simplified list CSS & removed dynamic text handling of static text.

--- src/components/EvacAssist.vue
+++ src/components/EvacAssist.vue
@@ -10,32 +10,39 @@ and lists, interactive elements and so forth are center justified.-->
         </div>
       </div>
 
-
-
-        <div class="boxed" id="introduction-field">
-            <h3>Introduction</h3>
+      <div class="boxed text-box" id="introduction-field">
+        <h3>Introduction</h3>
         <p>
-            It may be necessary to evacuate a building, room, or space due to an emergency incident or hazardous, situation.
-            Preparing for an evacuation is a shared responsibility among individuals, the University, and first responders.<br><br>
-            All people, including individuals with disabilities and others with access and functional needs, are strongly encouraged to develop a personal emergency plan.
-            PSU’s Environmental Health and Safety department offers a
-            <a href="https://www.pdx.edu/environmental-health-safety/emergency-planning-for-people-needing-assistance" target="_blank" rel="noopener noreferrer">resource page</a>
-            on their website, including
-            <a href="https://www.pdx.edu/environmental-health-safety/sites/www.pdx.edu.environmental-health-safety/files/ada.emergency.guidelines_2.pdf" target="_blank" rel="noopener noreferrer">guidelines for developing a Personal Emergency Plan</a>
-            that will help you to prepare. Included within the guidelines is information regarding evacuation strategies;
-            areas of refuge; suggested guidelines for specific mobility limitations and additional on-campus resources.
+          It may be necessary to evacuate a building, room, or space due to an emergency incident or hazardous, situation.
+          Preparing for an evacuation is a shared responsibility among individuals, the University, and first responders.<br><br>
+          All people, including individuals with disabilities and others with access and functional needs, are strongly encouraged to develop a personal emergency plan.
+          PSU’s Environmental Health and Safety department offers a
+          <a href="https://www.pdx.edu/environmental-health-safety/emergency-planning-for-people-needing-assistance" target="_blank" rel="noopener noreferrer">resource page</a>
+          on their website, including
+          <a href="https://www.pdx.edu/environmental-health-safety/sites/www.pdx.edu.environmental-health-safety/files/ada.emergency.guidelines_2.pdf" target="_blank" rel="noopener noreferrer">guidelines for developing a Personal Emergency Plan</a>
+          that will help you to prepare. Included within the guidelines is information regarding evacuation strategies;
+          areas of refuge; suggested guidelines for specific mobility limitations and additional on-campus resources.
         </p>
-        </div>
+      </div>
 
-        <div class="boxed" id="about-field">
+
+
+
+
+        <div class="boxed text-box">
             <h3>About this registry</h3>
-        <p>
-            {{about_registry_text1}}
-            <ul class="c">
+            <p>
+              PSU has developed this registry as a mechanism for individuals with a disability
+              (or an access or functional need) that would prevent them from independently exiting a building during
+              an emergency evacuation. If you register, your information will become accessible to first responders
+              (information could include, as applicable: name, assigned residence hall room (PSU residents only),
+              location of registered courses (student or instructor/faculty), assigned office location (staff),
+              and/or contact information) The registry is confidential and is managed by CPSO. Data is refreshed weekly.
+            <ul>
                 Please note:
-                <ul class="b">
+                <ul>
                     <li>This registry IS NOT a way to get advance notice of fire alarm tests or drills.</li>
-                    <ul class="a">
+                    <ul>
                         <li>In PSU residence halls, fire drill tests are conducted once per term. Contact your residence hall
                             advisor if you need advance notice of those tests.</li>
                         <li>In all other PSU buildings, fire alarm testing is most often done outside of normal business hours.
@@ -43,9 +50,9 @@ and lists, interactive elements and so forth are center justified.-->
                     </ul>
                 </ul>
 
-                <ul class="b">
+                <ul>
                     <li>This registry IS NOT a guarantee of immediate assistance.</li>
-                    <ul class="a">
+                    <ul>
                         <li>Emergency personnel will assess the specific incident that occurs and apply the appropriate
                             protocol for stabilizing the incident and providing assistance. This registry is one piece of
                             information they can consider in their overall assessment of the incident.</li>
@@ -132,13 +139,6 @@ and lists, interactive elements and so forth are center justified.-->
         },
         //This initializes the above declarations
         created: function() {
-            //This is one way to handle massive text blobs in a template but is not strictly necessary
-            this.about_registry_text1 = "PSU has developed this registry as a mechanism for individuals with a disability" +
-                " (or an access or functional need) that would prevent them from independently exiting a building during" +
-                " an emergency evacuation. If you register, your information will become accessible to first responders" +
-                " (information could include, as applicable: name, assigned residence hall room (PSU residents only)," +
-                " location of registered courses (student or instructor/faculty), assigned office location (staff)," +
-                " and/or contact information) The registry is confidential and is managed by CPSO. Data is refreshed weekly. ",
             this.regbox = false
         },
         //This is for button clicks
@@ -172,18 +172,21 @@ and that is pretty standard amongst other CSS properties it seems-->
         padding: 0;
         font-size: 18px;
     }
-    ul.a { /*unordered list*/
-        list-style-type: circle; /*what are the bullets? Here they are empty circles*/
-        font-weight: normal;
+
+    ul {
+      list-style-type: none;
+      font-weight: bold;
     }
-    ul.b { /*unordered list*/
-        list-style-type: disc; /*what are the bullets? Here they are filled circles*/
-        font-weight: bold;
+
+    ul > ul {
+      list-style-type: disc;
+      font-weight: bold;
     }
-    ul.c { /*unordered list*/
-        list-style-type: none; /*what are the bullets? Here they are nothing*/
-        font-weight: bold;
+    ul > ul > ul {
+      list-style-type: disc;
+      font-weight: normal;
     }
+
     ul.d { /*unordered list*/
         list-style-type: disc; /*what are the bullets? Here they are filled circles*/
         font-weight: normal;
@@ -194,11 +197,6 @@ and that is pretty standard amongst other CSS properties it seems-->
         color: #42b983;
     }
 
-    #introduction-field{
-        margin: auto;
-        margin-bottom: 30px;
-        padding: 20px;
-    }
 
     #about-field{
         margin: auto;
