63aefaabceab79b5b70d1c35b017d6d339cdadf4
Trevor Leake <contact@trevorleake.com>
2019-07-24 23:12:27 -0700

    Fix "auth loss on refresh" bug

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import styles from './styles/styles.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+// Set our headers to include Auth tokens by default
+if(localStorage.getItem('token'))
+  axios.defaults.headers.common['Authorization'] = localStorage.getItem('token')
 
 new Vue({
   router,
--- src/store.js
+++ src/store.js
@@ -13,10 +13,9 @@ export default new Vuex.Store({
     auth_request(state) {
       state.status = 'loading'
     },
-    auth_success(state, token, user) {
+    auth_success(state, token) {
       state.status = 'success'
       state.token = token
-      state.user = user
     },
     auth_error(state) {
       state.status = 'error'
@@ -32,25 +31,19 @@ export default new Vuex.Store({
       // Attempt login
       axios({url: 'http://localhost:8000/login/', data: user, method: 'POST' })
       .then(resp => {
-        alert("Our token is: " + JSON.stringify(resp))
-
         // Extract token and save in Vuex store
         const token = resp.data
         localStorage.setItem('token', token)
-
-        // Set our headers to include Auth tokens by default
-        axios.defaults.headers.common['Authorization'] = localStorage.getItem('token')
 
         // Commit the state to the Vuex store
         commit('auth_success', token)
       })
       .catch(err => {
         commit('auth_error')
-        localStorage.removeItem('token')
+//        localStorage.removeItem('token')
         reject(err)
-        alert("Path 1")
       })
-      })
+    })
     }
   },
   getters: {
