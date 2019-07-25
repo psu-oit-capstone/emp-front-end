3c9128474f5a3584deaa3cd2a9138724eb4a24e2
Trevor Leake <contact@trevorleake.com>
2019-07-24 23:20:05 -0700

    Add logout action

--- src/store.js
+++ src/store.js
@@ -23,27 +23,35 @@ export default new Vuex.Store({
   },
   actions: {
     // Called from the LoginForm
-    login({commit}, user){
-    return new Promise((resolve, reject) => {
-      // Temporary state
-      commit('auth_request');
+    login({commit}, user) {
+      return new Promise((resolve, reject) => {
+        // Temporary state
+        commit('auth_request');
 
-      // Attempt login
-      axios({url: 'http://localhost:8000/login/', data: user, method: 'POST' })
-      .then(resp => {
-        // Extract token and save in Vuex store
-        const token = resp.data
-        localStorage.setItem('token', token)
+        // Attempt login
+        axios({url: 'http://localhost:8000/login/', data: user, method: 'POST' })
+        .then(resp => {
+          // Extract token and save in Vuex store
+          const token = resp.data
+          localStorage.setItem('token', token)
 
-        // Commit the state to the Vuex store
-        commit('auth_success', token)
+          // Commit the state to the Vuex store
+          commit('auth_success', token)
+        })
+        .catch(err => {
+          commit('auth_error')
+          localStorage.removeItem('token')
+          reject(err)
+        })
       })
-      .catch(err => {
-        commit('auth_error')
-//        localStorage.removeItem('token')
-        reject(err)
+    },
+    logout({commit}){
+      return new Promise((resolve, reject) => {
+        commit('logout')
+        localStorage.removeItem('token')
+        delete axios.defaults.headers.common['Authorization']
+        resolve()
       })
-    })
     }
   },
   getters: {
