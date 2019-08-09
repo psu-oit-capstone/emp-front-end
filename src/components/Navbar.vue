1546f1993693ed4011675778ca11e9fadcfc3a14
trevorleake <trevorjohnleake@gmail.com>
2019-08-09 15:35:41 -0700

    Move Navbar into its own component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- Nav bar -->
+  <div id="header-wrap">
+    <div id="header">
+
+      <ul id="site-navigation" class="nav pull-right">
+        <li style="border-right:none;"><router-link to="/emergency-information">Emergency Information</router-link></li>
+        <li style="border-right:none;"><router-link to="/logout">Logout</router-link></li>
+      </ul>
+      <img src="../images/m-logo.png" class="logo pull-left" />
+
+    </div>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'Navbar'
+  }
+</script>
+
+
+<style scoped>
+</style>
